2a8bbec722ec11642184367a257a45608fd103fb
Zoltán Galáž <xgalaz00@gmail.com>
2021-11-26 11:36:00 +0100

    Create database directories in the path if needed

--- api/authentication/__init__.py
+++ api/authentication/__init__.py
@@ -1,5 +1,7 @@
+import os
+from pathlib import Path
 from flask_bcrypt import Bcrypt
-from api.configuration import load_configuration
+from api.configuration import application_path, load_configuration
 from api.authentication.database import initialize_database
 
 
@@ -16,6 +18,12 @@ def configure_authentication(app):
     # Configure the authentication database
     for key, value in load_configuration("authentication.json").get("database", {}).items():
         app.config[key] = value
+
+    # Get the database path
+    path = app.config["SQLALCHEMY_DATABASE_URI"].lstrip("sqlite:///").split("/")[:-1]
+
+    # Prepare the database path and make sure the database directory exists
+    Path(os.path.join(application_path, "..", *path)).mkdir(parents=True, exist_ok=True)
 
     # Initialize the authentication database
     initialize_database(app)
